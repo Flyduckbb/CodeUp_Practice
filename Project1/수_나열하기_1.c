cc5933a3969720eef64a700963c475898ba85cf9
Flyduckbb <gogo5208@gmail.com>
2021-01-21 20:01:25 +0900

    1089번

---
+++ Project1/수_나열하기_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a, d, n;
+
+	scanf("%d %d %d", &a, &d, &n);
+
+	for (int i = 1; i < n; i++)
+	{
+		a += d;
+	}
+
+	printf("%d", a);
+
+	return 0;
+}
