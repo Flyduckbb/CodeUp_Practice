c629933a4994d0f67c81036830334414626e1545
Flyduckbb <gogo5208@gmail.com>
2021-01-25 02:22:25 +0900

    fixing now

--- Project1/설탕과자_뽑기.c
+++ Project1/설탕과자_뽑기.c
@@ -3,5 +3,17 @@
 
 int main(void)
 {
+	int h, w, cnt;
+	int l, d, x, y;
+	int arry[100][100];
+
+	scanf("%d %d", &h, &w);
+	scanf("%d", &cnt);
+	scanf("%d %d %d %d", &l, &d, &x, &y);
+
+	for (int i = 0; i < cnt; i++)
+	{
+		
+	}
 
 }
