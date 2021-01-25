a2559b9b7983229fce70d5845f03b0ab22642641
Flyduckbb <gogo5208@gmail.com>
2021-01-26 02:16:00 +0900

    설탕과자_뽑기 ( 중요 )

--- Project1/설탕과자_뽑기.c
+++ Project1/설탕과자_뽑기.c
@@ -5,15 +5,33 @@ int main(void)
 {
 	int h, w, cnt;
 	int l, d, x, y;
-	int arry[100][100];
+	int arry[100][100] = { 0 };
 
 	scanf("%d %d", &h, &w);
 	scanf("%d", &cnt);
-	scanf("%d %d %d %d", &l, &d, &x, &y);
 
 	for (int i = 0; i < cnt; i++)
 	{
-		
+		scanf("%d %d %d %d", &l, &d, &x, &y);
+
+		if (d == 1)
+		{
+			for (int j = 0; j < l; j++)
+				arry[x+j-1][y-1] = 1;
+		}
+		else
+		{
+			for (int j = 0; j < l; j++)
+				arry[x-1][y+j-1] = 1;
+		}
 	}
 
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+			printf("%d ", arry[i][j]);
+		printf("\n");
+	}
+
+	return 0;
 }
