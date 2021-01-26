71b9767838c26c073e17647e3d744ebaecaad816
Flyduckbb <gogo5208@gmail.com>
2021-01-26 15:08:15 +0900

    1099번 ( 중요 )

--- Project1/성실한_개미.c
+++ Project1/성실한_개미.c
@@ -3,5 +3,40 @@
 
 int main(void)
 {
+	int map[10][10] = { 0 };
 
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+			scanf("%d", &map[i][j]);
+	}
+
+	int x = 1, y = 1;
+	for(int i=0; i<30; i++)
+	{
+		if (map[x][y] == 0)
+		{
+			map[x][y] = 9;
+			y++;
+		}
+		else if (map[x][y] == 1)
+		{
+			x++;
+			y--;
+		}
+		else if (map[x][y] == 2)
+		{
+			map[x][y] = 9;
+			break;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+			printf("%d ", map[i][j]);
+		printf("\n");
+	}
+
+	return 0;
 }
