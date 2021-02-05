9eb36c4bdeeb5b81a710c5803795ec855bf583c5
Flyduckbb <gogo5208@gmail.com>
2021-02-05 15:14:15 +0900

    CHECK POINT

--- Project1/3004_데이터_재정렬.c
+++ Project1/3004_데이터_재정렬.c
@@ -6,6 +6,38 @@ typedef struct data_rebuild {
 	int index;
 } DR;
 
+void qsort(int A[], int p, int r)
+{
+	int q;
+
+	if (p < r)
+	{
+		q = partition(A, p, r);
+		qsort(A, p, q - 1);
+		qsort(A, q + 1, r);
+	}
+}
+
+int partition(int B[], int pp, int rr)
+{
+	int x = B[rr];
+	int i = pp - 1;
+	for (int j = pp; j < rr; j++)
+	{
+		if (B[j] <= x)
+		{
+			int a = B[++i];
+			B[i] = B[j];
+			B[j] = a;
+		}
+
+	}
+	int b = B[i + 1];
+	B[i + 1] = B[rr];
+	B[rr] = b;
+	return i + 1;
+}
+
 int main(void)
 {
 	DR arry[50000];
@@ -19,7 +51,7 @@ int main(void)
 		arry[i].index = i;
 	}
 
-	q_srt(arry->data, 0, cnt - 1);
+	qsort(arry, 0, cnt - 1);
 
 	for (int i = 0; i < cnt; i++)
 		printf("%d ", arry[i].index);
