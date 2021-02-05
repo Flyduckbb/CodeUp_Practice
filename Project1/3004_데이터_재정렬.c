e87c644b8526a6cfb4d70b498e1f70ddc450c488
Flyduckbb <gogo5208@gmail.com>
2021-02-05 14:52:16 +0900

    rebuilding

--- Project1/3004_데이터_재정렬.c
+++ Project1/3004_데이터_재정렬.c
@@ -1,36 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct data_sort {
-	int data;
-	int index;
-} DS;
-
-void insert_data(int A[], int count)
-{
-	for (int i = 0; i < count; i++)
-	{
-		scanf("%d", &A[i].data);
-		A[i].index = i;
-	}
-}
-
-void print_data(int A[], int count)
-{
-	for (int i = 0; i < count; i++)
-		printf("%d ", A[i]);
-}
-
-int main(void)
-{
-	int cnt;
-	int DS[50000];
-
-	scanf("%d", &cnt);
-
-	insert_data(DS, cnt);
-
-	print_data(DS, cnt);
-
-	return 0;
-}
