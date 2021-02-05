b6cd20e38dfb10d3780c938e15655a54654bf191
Flyduckbb <gogo5208@gmail.com>
2021-02-05 15:06:14 +0900

    save

--- Project1/3004_데이터_재정렬.c
+++ Project1/3004_데이터_재정렬.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct data_rebuild {
+	int data;
+	int index;
+} DR;
+
+int main(void)
+{
+	DR arry[50000];
+	int cnt;
+
+	scanf("%d", &cnt);
+
+	for (int i = 0; i < cnt; i++)
+	{
+		scanf("%d", &arry[i].data);
+		arry[i].index = i;
+	}
+
+	q_srt(arry->data, 0, cnt - 1);
+
+	for (int i = 0; i < cnt; i++)
+		printf("%d ", arry[i].index);
+
+	return 0;
+}
